a4878bd4bc298586ad1f8ba03fa6492727153a77
OGG <oks2.6@yandex.ru>
2021-04-18 18:38:10 +0300

    edit

--- src/test/java/tests/CucumberLoginTest.java
+++ src/test/java/tests/CucumberLoginTest.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Given;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.qameta.allure.Step;
 import org.junit.After;
 
 import page.LoginPage;
@@ -13,7 +14,7 @@ import java.net.MalformedURLException;
 
 public class CucumberLoginTest extends LoginPage {
     LoginPage loginPage;
-
+@Step("test")
     @Given("^Открываем страницу$")
     public void open() throws MalformedURLException {
         selenideOpen();
