c2ac6d57768c099d72aeb5fe423db5a40714416d
OGG <oks2.6@yandex.ru>
2021-04-18 03:52:53 +0300

    edit

--- src/main/java/page/BooksPage.java
+++ src/main/java/page/BooksPage.java
@@ -5,7 +5,6 @@ import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
